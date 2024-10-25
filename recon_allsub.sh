28688837d41e9615d9ff2eefc7ffaed691227c6b
jiajeng <jang05213127@gmail.com>
2024-10-25 11:15:19 +0800

    command code to run all subject

---
+++ recon_allsub.sh
@@ -0,0 +1,15 @@
+ISUBJECT_DIR=/media/sf_MRI_T1
+NII_DIR=/T1nii
+directory=$ISUBJECT_DIR
+SUBJECTS_DIR="/media/sf_MRI_T1/recon/"
+
+if [ ! -d "$directory" ]; then
+  exit 1
+fi
+
+for file in $(find ${directory}/SUB* -maxdepth 0 -type d -exec basename {} \;); do
+    FILE_DIR=$directory"/"$file$NII_DIR
+    for niifile in "${FILE_DIR}"/*".nii"; do
+    	recon-all -subjid ${file} -i ${niifile} -all
+    done
+done
