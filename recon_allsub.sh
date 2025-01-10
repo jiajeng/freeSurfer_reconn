1b9424d0067bdb67aea9a2b3b07eb374b214ca7b
jiajeng <jang05213127@gmail.com>
2025-01-10 13:07:32 +0800

    Add comment

--- recon_allsub.sh
+++ recon_allsub.sh
@@ -1,12 +1,21 @@
+# ISUBJECT_DIR : subject dir
+# NII_DIR : under subject dir, the nii file folder dir
+# SUBJECTS_DIR : result folder
 ISUBJECT_DIR=/media/sf_MRI_T1
 NII_DIR=/T1nii
 directory=$ISUBJECT_DIR
 SUBJECTS_DIR="/media/sf_MRI_T1/recon/"
 
+
 if [ ! -d "$directory" ]; then
   exit 1
 fi
 
+# $(find ${directory}/SUB* -maxdepth 0 -type d -exec basename {} \;) : ls(fullfile(directory,'SUB*'))
+#		        ^
+#			      |
+#			      |
+#			change this(filename pattern e.x. SUB*)
 for file in $(find ${directory}/SUB* -maxdepth 0 -type d -exec basename {} \;); do
     FILE_DIR=$directory"/"$file$NII_DIR
     for niifile in "${FILE_DIR}"/*".nii"; do
